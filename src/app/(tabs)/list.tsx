b2deb1068f33b0ceccbcaad8b16bb0e1fa341dbf
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2024-11-15 22:18:10 +0900

    feat: navigation bar

---
+++ src/app/(tabs)/list.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+import { Bold } from "@/components/common/Bold";
+import { Button } from "@/components/common/Button";
+import { ThemedText } from "@/components/common/ThemedText";
+import { ThemedView } from "@/components/common/ThemedView";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ListScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ThemedText type="title1" style={{ marginBottom: 8 }}>
+        가전제품 목록 페이지입니다.
+      </ThemedText>
+      <ThemedView>
+        <ThemedText type="title1" style={{ marginBottom: 16 }}>
+          볼드체를 사용하기 위해서는 <Bold type="title1">볼드 컴포넌트</Bold>를
+          사용하세요.
+        </ThemedText>
+        <Button variant="filled" onPress={() => {}}>
+          Open Developer Tools
+        </Button>
+      </ThemedView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 32,
+    flex: 1,
+    justifyContent: "center",
+  },
+});
---
+++ src/components/icons/Home.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+interface IconProps {
+  color: string;
+  width: number;
+  height: number;
+}
+
+const HomeIcon = ({ width, height, color }: IconProps) => (
+  <Svg width={width} height={height} fill="none">
+    <Path
+      stroke={color}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m9.02 2.84-5.39 4.2C2.73 7.74 2 9.23 2 10.36v7.41c0 2.32 1.89 4.22 4.21 4.22h11.58c2.32 0 4.21-1.9 4.21-4.21V10.5c0-1.21-.81-2.76-1.8-3.45l-6.18-4.33c-1.4-.98-3.65-.93-5 .12ZM12 17.99v-3"
+    />
+  </Svg>
+);
+export default HomeIcon;
