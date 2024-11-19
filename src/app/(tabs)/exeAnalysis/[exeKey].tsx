cb6aeafcc6c9a6e3d612f36bee50a1e650199426
hjin03 <cats55655@naver.com>
2024-11-20 04:05:25 +0900

    feat: animated number effect in analysis detail page

--- src/app/(tabs)/exeAnalysis/[exeKey].tsx
+++ src/app/(tabs)/exeAnalysis/[exeKey].tsx
@@ -45,7 +45,7 @@ export default function ExeAnalysisDetail() {
     return result.trim();
   };
 
-  const formattedDay = formatDateToKorean(day as string); // 날짜 포맷 적용
+  const formattedDay = formatDateToKorean(day as string);
   const exeHistory = JSON.parse(exeHistoryString as string) as ExeHistory[];
   const totalWh = exeHistory.reduce(
     (sum, history) => sum + history.eleUsage.Wh,
@@ -61,26 +61,22 @@ export default function ExeAnalysisDetail() {
   );
   return (
     <ScrollView style={styles.container}>
-      <ThemedView style={styles.section}>
+      <ThemedView style={(styles.section, { flexDirection: "column", gap: 4 })}>
         <ThemedText type="body">
           {formattedDay} | {formatDuration(totalDuration)}
         </ThemedText>
         <ThemedText type="title1">{content}</ThemedText>
         <ThemedText type="body">주어진 임무를 완벽하게 마쳤어요!</ThemedText>
       </ThemedView>
-      <ThemedView
-        style={{ alignItems: "center", marginTop: 48, marginBottom: 16 }}
-      >
+      <ThemedView style={{ alignItems: "center", marginVertical: 32 }}>
         <EnergyCard
           fare={totalFare}
           totalEnergy={totalWh}
           Analogy="막대사탕 1개"
         />
       </ThemedView>
-      <ThemedView style={styles.section}></ThemedView>
-
       <ThemedView style={styles.section}>
-        <ThemedText type="headline" style={styles.sectionTitle}>
+        <ThemedText type="headline" style={{ marginBottom: 16 }}>
           실행 이력
         </ThemedText>
         <ThemedView style={styles.contentSection}>
@@ -110,43 +106,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    padding: 24,
+    paddingHorizontal: 24,
   },
   section: {
     marginBottom: 16,
   },
-  sectionTitle: {
-    marginBottom: 16,
-  },
-  // infoBox: {
-  //   flexDirection: "row",
-  //   justifyContent: "space-between",
-  //   backgroundColor: "#F8F8F8",
-  //   padding: 20,
-  //   borderRadius: 16,
-  // },
-  // infoItem: {
-  //   alignItems: "center",
-  //   gap: 8,
-  // },
-  // historyItem: {
-  //   backgroundColor: "#F8F8F8",
-  //   padding: 20,
-  //   borderRadius: 16,
-  //   marginBottom: 12,
-  // },
-  // historyHeader: {
-  //   flexDirection: "row",
-  //   justifyContent: "space-between",
-  //   alignItems: "center",
-  //   marginBottom: 8,
-  // },
-  // status: {
-  //   fontWeight: "600",
-  // },
-  // duration: {
-  //   color: Colors.light.lowGray,
-  // },
   contentSection: {
     marginVertical: 4,
     gap: 16,
@@ -156,226 +120,3 @@ const styles = StyleSheet.create({
     marginBottom: 42,
   },
 });
-// import { useLocalSearchParams } from "expo-router";
-// import { StyleSheet, ScrollView } from "react-native";
-// import { ThemedView } from "@/components/common/ThemedView";
-// import { ThemedText } from "@/components/common/ThemedText";
-// import { Colors } from "@/constants/colors.constant";
-
-// interface ExeHistory {
-//   startTime: string;
-//   duration: number;
-//   name: string;
-//   status: "success" | "failure";
-// }
-
-// export default function ExeAnalysisDetail() {
-//   const {
-//     day,
-//     content,
-//     exeHistory: exeHistoryString,
-//     exeKey,
-//   } = useLocalSearchParams();
-
-//   const exeHistory = JSON.parse(exeHistoryString as string) as ExeHistory[];
-
-//   return (
-//     <ScrollView style={styles.container}>
-//       <ThemedView style={styles.section}>
-//         <ThemedText type="body">{day}</ThemedText>
-//         <ThemedText type="title1">{content}</ThemedText>
-//         <ThemedText type="body">주어진 임무를 완벽하게 마쳤어요!</ThemedText>
-//       </ThemedView>
-
-//       <ThemedView style={styles.section}>
-//         <ThemedText type="headline" style={styles.sectionTitle}>
-//           실행 이력
-//         </ThemedText>
-//         {exeHistory.map((history, index) => (
-//           <ThemedView key={index} style={styles.historyItem}>
-//             <ThemedView style={styles.historyHeader}>
-//               <ThemedText type="headline">{history.name}</ThemedText>
-//               <ThemedText
-//                 type="callout"
-//                 style={[
-//                   styles.status,
-//                   {
-//                     color:
-//                       history.status === "success"
-//                         ? Colors.light.tint
-//                         : Colors.light.black,
-//                   },
-//                 ]}
-//               >
-//                 {history.status === "success" ? "성공" : "실패"}
-//               </ThemedText>
-//             </ThemedView>
-//             <ThemedText type="body" style={styles.duration}>
-//               실행 시간: {Math.floor(history.duration / 60)}분
-//             </ThemedText>
-//           </ThemedView>
-//         ))}
-//       </ThemedView>
-//     </ScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "white",
-//     padding: 24,
-//   },
-//   section: {
-//     marginBottom: 32,
-//   },
-//   sectionTitle: {
-//     marginBottom: 16,
-//   },
-//   historyItem: {
-//     backgroundColor: "#F8F8F8",
-//     padding: 20,
-//     borderRadius: 16,
-//     marginBottom: 12,
-//   },
-//   historyHeader: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     alignItems: "center",
-//     marginBottom: 8,
-//   },
-//   status: {
-//     fontWeight: "600",
-//   },
-//   duration: {
-//     color: Colors.light.lowGray,
-//   },
-// });
-
-// // import { useLocalSearchParams } from "expo-router";
-// // import { StyleSheet, ScrollView } from "react-native";
-// // import { ThemedView } from "@/components/common/ThemedView";
-// // import { ThemedText } from "@/components/common/ThemedText";
-// // import { Colors } from "@/constants/colors.constant";
-
-// // export default function ExeAnalysisDetail() {
-// //   const params = useLocalSearchParams();
-// //   const content = params.content as string;
-// //   const eleUsageWh = Number(params.eleUsageWh);
-// //   const eleUsageFare = Number(params.eleUsageFare);
-// //   const exeHistory = JSON.parse(params.exeHistory as string);
-// //   console.log(exeHistory);
-// //   return (
-// //     <ScrollView style={styles.container}>
-// //       <ThemedView style={styles.section}>
-// //         <ThemedText type="body">
-// //           {exeHistory[0].startTime.split(" ")[0].split("-")[0]}년{" "}
-// //           {exeHistory[0].startTime.split(" ")[0].split("-")[1]}월{" "}
-// //           {exeHistory[0].startTime.split(" ")[0].split("-")[2]}일 |{" "}
-// //           {Math.floor(exeHistory[0].duration / 3600)
-// //             ? `${Math.floor(exeHistory[0].duration / 3600)}시간 `
-// //             : ""}
-// //           {Math.floor((exeHistory[0].duration % 3600) / 60)
-// //             ? `${Math.floor((exeHistory[0].duration % 3600) / 60)}분 `
-// //             : ""}
-// //           {exeHistory[0].duration % 60
-// //             ? `${exeHistory[0].duration % 60}초`
-// //             : ""}
-// //         </ThemedText>
-// //         <ThemedText type="title1">{content}</ThemedText>
-// //         <ThemedText type="body">주어진 임무를 완벽하게 마쳤어요!</ThemedText>
-// //       </ThemedView>
-
-// //       <ThemedView style={styles.section}>
-// //         <ThemedText type="headline" style={styles.sectionTitle}>
-// //           전력 사용량
-// //         </ThemedText>
-// //         <ThemedView style={styles.infoBox}>
-// //           <ThemedView style={styles.infoItem}>
-// //             <ThemedText type="body">총 사용량</ThemedText>
-// //             <ThemedText type="headline">{eleUsageWh}Wh</ThemedText>
-// //           </ThemedView>
-// //           <ThemedView style={styles.infoItem}>
-// //             <ThemedText type="body">예상 요금</ThemedText>
-// //             <ThemedText type="headline">
-// //               {eleUsageFare.toLocaleString()}원
-// //             </ThemedText>
-// //           </ThemedView>
-// //         </ThemedView>
-// //       </ThemedView>
-
-// //       <ThemedView style={styles.section}>
-// //         <ThemedText type="headline" style={styles.sectionTitle}>
-// //           실행 이력
-// //         </ThemedText>
-// //         {exeHistory.map((history: any, index: number) => (
-// //           <ThemedView key={index} style={styles.historyItem}>
-// //             <ThemedView style={styles.historyHeader}>
-// //               <ThemedText type="headline">{history.name}</ThemedText>
-// //               <ThemedText
-// //                 type="callout"
-// //                 style={[
-// //                   styles.status,
-// //                   {
-// //                     color:
-// //                       history.status === "success"
-// //                         ? Colors.light.tint
-// //                         : Colors.light.black,
-// //                   },
-// //                 ]}
-// //               >
-// //                 {history.status === "success" ? "성공" : "실패"}
-// //               </ThemedText>
-// //             </ThemedView>
-// //             <ThemedText type="body" style={styles.duration}>
-// //               실행 시간: {Math.floor(history.duration / 60)}분
-// //             </ThemedText>
-// //           </ThemedView>
-// //         ))}
-// //       </ThemedView>
-// //     </ScrollView>
-// //   );
-// // }
-
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: "white",
-// //     padding: 24,
-// //   },
-// //   section: {
-// //     marginBottom: 32,
-// //   },
-// //   sectionTitle: {
-// //     marginBottom: 16,
-// //   },
-// //   infoBox: {
-// //     flexDirection: "row",
-// //     justifyContent: "space-between",
-// //     backgroundColor: "#F8F8F8",
-// //     padding: 20,
-// //     borderRadius: 16,
-// //   },
-// //   infoItem: {
-// //     alignItems: "center",
-// //     gap: 8,
-// //   },
-// //   historyItem: {
-// //     backgroundColor: "#F8F8F8",
-// //     padding: 20,
-// //     borderRadius: 16,
-// //     marginBottom: 12,
-// //   },
-// //   historyHeader: {
-// //     flexDirection: "row",
-// //     justifyContent: "space-between",
-// //     alignItems: "center",
-// //     marginBottom: 8,
-// //   },
-// //   status: {
-// //     fontWeight: "600",
-// //   },
-// //   duration: {
-// //     color: Colors.light.lowGray,
-// //   },
-// // });
--- src/components/analysis/energyCard.tsx
+++ src/components/analysis/energyCard.tsx
@@ -3,20 +3,44 @@ import { ThemedText } from "../common/ThemedText";
 import { ThemedView } from "../common/ThemedView";
 import { StyleSheet, Image } from "react-native";
 import CoinImage from "@/assets/images/coin.png";
-
+import AnimatedNumbers from "react-native-animated-numbers";
+import { useEffect, useState } from "react";
 interface energyCardProps {
   fare: number;
   totalEnergy: number;
   Analogy: string;
 }
 export const EnergyCard = ({ fare, totalEnergy, Analogy }: energyCardProps) => {
+  const [animateToNumber, setAnimateToNumber] = useState(0);
+  useEffect(() => {
+    setAnimateToNumber(fare);
+  }, [fare]);
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.title}>
         <ThemedText type="body">전력사용량</ThemedText>
-        <ThemedText type="headline">{fare}원</ThemedText>
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            alignItems: "baseline",
+            justifyContent: "center",
+          }}
+        >
+          <AnimatedNumbers
+            includeComma
+            animateToNumber={animateToNumber}
+            fontStyle={{
+              fontSize: 17,
+              fontWeight: "600",
+              lineHeight: 22,
+              fontFamily: "LGEIHeadline-SemiBold",
+            }}
+          />
+          <ThemedText type="headline">원</ThemedText>
+        </ThemedView>
         <ThemedText type="body">{totalEnergy}Wh</ThemedText>
       </ThemedView>
+
       <ThemedView style={styles.image}>
         <Image source={CoinImage} style={{ width: 105, height: 105 }}></Image>
       </ThemedView>
@@ -46,7 +70,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    // justifyContent: "space-between",
   },
   bottomText: {
     alignItems: "center",
