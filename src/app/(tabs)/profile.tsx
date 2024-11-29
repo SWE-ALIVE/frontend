f26387f7dc31a371de4ac8abcd2c254e9ff18642
hjin03 <cats55655@naver.com>
2024-11-29 11:16:46 +0900

    chore: simple profile

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -3,9 +3,9 @@ import { StyleSheet } from "react-native";
 import { Button } from "@/components/common/Button";
 import { ThemedText } from "@/components/common/ThemedText";
 import { ThemedView } from "@/components/common/ThemedView";
+import { Colors } from "@/constants/colors.constant";
 import { useUserStore } from "@/stores/useUserStore";
 import { useRouter } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileScreen() {
   const { clearUser, user } = useUserStore();
@@ -16,35 +16,69 @@ export default function ProfileScreen() {
     router.push("/");
   };
   return (
-    <SafeAreaView style={styles.container}>
-      <ThemedText type="title1" style={{ marginBottom: 8 }}>
-        프로필페이지입니다.
-      </ThemedText>
-      <ThemedView>
-        <ThemedText type="title1" style={{ marginBottom: 16 }}>
-          정보
+    <ThemedView style={styles.container}>
+      <ThemedView style={{ paddingHorizontal: 32 }}>
+        <ThemedText type="title1" style={{ marginBottom: 8, height: 80 }}>
+          프로필
         </ThemedText>
-        <ThemedText type="title1" style={{ marginBottom: 16 }}>
+        <ThemedText type="title3" style={{ marginBottom: 16 }}>
           {user?.name}
         </ThemedText>
-        <ThemedText type="title1" style={{ marginBottom: 16 }}>
+        <ThemedText type="title3" style={{ marginBottom: 16 }}>
           {user?.phone_number}
         </ThemedText>
-        <ThemedText type="title1" style={{ marginBottom: 16 }}>
-          로그아웃
-        </ThemedText>
-        <Button variant="filled" onPress={logout}>
-          Open Developer Tools
-        </Button>
       </ThemedView>
-    </SafeAreaView>
+      <ThemedView
+        style={{ backgroundColor: Colors.light.buttonDisabled, flex: 1 }}
+      >
+        <ThemedView
+          style={{
+            padding: 32,
+            backgroundColor: Colors.light.buttonDisabled,
+          }}
+        >
+          <ThemedView
+            style={{
+              height: 32,
+              flex: 1,
+              backgroundColor: "white",
+              paddingVertical: 40,
+              borderRadius: 12,
+              marginBottom: 12,
+            }}
+          ></ThemedView>
+          <ThemedView
+            style={{
+              height: 32,
+              flex: 1,
+              backgroundColor: "white",
+              paddingVertical: 40,
+              borderRadius: 12,
+              marginBottom: 12,
+            }}
+          ></ThemedView>
+          <ThemedView
+            style={{
+              height: 32,
+              flex: 1,
+              backgroundColor: "white",
+              paddingVertical: 40,
+              borderRadius: 12,
+              marginBottom: 24,
+            }}
+          ></ThemedView>
+          <Button variant="filled" onPress={logout}>
+            로그아웃
+          </Button>
+        </ThemedView>
+      </ThemedView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 32,
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: Colors.light.background,
   },
 });
